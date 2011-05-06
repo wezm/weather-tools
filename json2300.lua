a59d7df80f3c0da210d045f80caaacbbf62865cb
Wesley Moore <wez@wezm.net>
2011-05-06 21:41:10 +1000

    Add extremes to the output

--- json2300.lua
+++ json2300.lua
@@ -1,5 +1,5 @@
 --[[
-# Copyright (c) 2010 Wesley Moore http://www.wezm.net/
+# Copyright (c) 2011 Wesley Moore http://www.wezm.net/
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -36,16 +36,60 @@ local db = sqlite3.open(db_path)
 local weather = weatherdb.new(db)
 
 local data = {}
-data.current = weather:current()
-data.current.min = weather:min()
-data.current.max = weather:max()
+local extremes = {}
+
 data.history = weather:history()
+data.current = weather:current()
 data.rain = {
   today = weather:rainfall_today(),
   history = weather:rainfall_history()
 }
 
 db:close()
+
+-- Add min, max values for each day in the history.
+-- Not using SQL because the history has had erroneous values filtered out
+-- local current = data.current
+for i, row in ipairs(data.history) do
+  local timestamp, inside, outside = row[1], row[2], row[3]
+
+  -- Get the date part of this timestamp
+  local date = os.date("!*t", timestamp / 1000)
+  date.hour, date.min, date.sec = 0, 0, 0;
+  local datestamp = os.time(date) * 1000
+
+  local current = extremes[datestamp]
+  if current == nil then
+  	current = { date = datestamp }
+  	extremes[datestamp] = current
+  end
+
+  if current.min == nil or outside < current.min.temperature then
+    current.min = {
+      temperature = outside,
+      timestamp   = timestamp
+    }
+  end
+  if current.max == nil or outside > current.max.temperature then
+    current.max = {
+      temperature = outside,
+      timestamp   = timestamp
+    }
+  end
+end
+
+-- Get the values of the extremes table
+local values = {}
+for timestamp, minmax in pairs(extremes) do
+  values[#values+1] = minmax
+end
+
+function comp(a, b)
+  return a.date < b.date
+end
+
+table.sort(values, comp)
+data.extremes = values
 
 -- write out JSON
 local jsonfile, err = io.open(json_path, "w")
@@ -55,3 +99,4 @@ if (jsonfile) then
 else
   print("Unable to open JSON file for writing: " .. err)
 end
+
